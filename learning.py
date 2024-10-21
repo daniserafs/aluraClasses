fc89c1a4cd4740d3e63c25fd3b3bcf2b2596d65c
Daniela Serafim <danyelaserafim@gmail.com>
2024-10-21 11:03:40 -0300

    primeiros passos python

---
+++ learning.py
@@ -0,0 +1,43 @@
+import math
+
+
+departamento = input("Digite o nome do departamento: ")
+responsavel = input("Digite o nome da pessoa responsável: ")
+print("O departamento de " + departamento + " é liderado por " + responsavel + ".")
+print('teste', '1', '2', '3')
+print ('''
+       teste
+       1
+       2
+       3
+       ''')
+print("teste", "1", "2", "3", sep='\n')
+print('Sabor Express')
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcao_escolhida = input('Escolha uma opção: ')
+
+print(f'Você escolheu a opção: {opcao_escolhida}')
+
+print('Python na Escola de Programação da Alura')
+nome = input('Digite seu nome: ')
+idade = input('Digite sua idade: ')
+print(f'Meu nome é {nome} e tenho {idade} anos')
+print("""
+      🅰
+      🅻
+      🆄
+      🆁
+      🅰
+
+      """)
+pi = 3.14159 * 100
+pi_arredondado =  math.floor(pi) / 100
+print(f'O valor arredondado de pi é: {pi_arredondado}')
+
+print('teste', '1', '2', '3')
+
