02364ad2d6f00ca190e9a7256537e7c90f314973
Daniela Serafim <danyelaserafim@gmail.com>
2024-10-24 09:41:34 -0300

    verificar status

--- Control.py
+++ Control.py
@@ -72,7 +72,21 @@ def atualizar_livro():
     print(livro)
     livro['preco'] = 69.90
     print(livro)
+    livro.update({'preco': 79.90})
+    print(livro)
 
+
+def verificar_status():
+    credenciais_clientes = {
+        'alice123':{'username':'alice123', 'password':'alic3P@ssw0rd', 'status' : 'active'},
+        'bob456':{'username':'bob456', 'password':'b0bP@ssword!', 'status' : 'inactive'},
+        'charlie789':{'username':'charlie789', 'password':'Ch@rlieP@ss9', 'status' : 'active'},
+    }
+
+    alerta = 'Enviar alerta' if credenciais_clientes['bob456']['status'] == 'inactive' else 'Sem alerta'
+    print(alerta)
+
+verificar_status()
 atualizar_livro()
 mostrar_projetos()
 mostrar_encomendas()
