188addd57f2f36379348d1d60456214acef55e3b
Daniela Serafim <danyelaserafim@gmail.com>
2024-10-25 11:01:38 -0300

    Music Object

---
+++ ObjectOrientation.py
@@ -0,0 +1,20 @@
+class Music:
+    name = ''
+    artist = ''
+    length = ''
+
+
+music_the_astronaut = Music()
+music_the_astronaut.name = 'The Astronaut'
+music_the_astronaut.artist = 'Jin'
+music_the_astronaut.length = '4.43'
+
+music_come_back_to_me = Music()
+music_come_back_to_me.name = 'Come back to me'
+music_come_back_to_me.artist = 'RM'
+music_come_back_to_me.length = '6.29'
+
+music_apt = Music()
+music_apt.name = 'APT.'
+music_apt.artist = 'ROSÉ and Bruno Mars'
+music_apt.length = '2.54'
