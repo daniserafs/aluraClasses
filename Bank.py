f18b88bc38e6a0c25bf96bd790ba61e8eb251c6f
Daniela Serafim <danyelaserafim@gmail.com>
2024-10-31 10:29:44 -0300

    BankAccount class

---
+++ Bank.py
@@ -0,0 +1,23 @@
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+        self._active = False
+    
+    def __str__(self):
+        return f'Hello, {self.owner}. Your account balance is R${self.balance}'
+    
+    @classmethod
+    def activate_account(cls, account):
+        account._active = True
+
+account1 = BankAccount('Daniela', 100)
+account2 = BankAccount('Jimin', 1000000000)
+print(account1)
+print(account2)
+account3 = BankAccount('RM', 10000000000)
+print(f'Before activating: Is account active? {account3._active}')
+BankAccount.activate_account(account3)
+print(f'After activating: Is account active? {account3._active}')
+
+
